1b2207e3a798847e196b9bdbf5f765a48b82e225
Srishti <Srishtishetty21@gmai.com>
2026-04-28 18:17:25 +0530

    Added Day 6 programs

---
+++ Day 6/day6_p1.py
@@ -0,0 +1,22 @@
+'''SQUARE
+******
+******
+******
+******
+******'''
+
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+'''OUTPUT:
+PS C:\Users\srish\OneDrive\Desktop\Logic Buliding> cd "Day 6"                                                              
+PS C:\Users\srish\OneDrive\Desktop\Logic Buliding\Day 6> python day6_p1.py                                                 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * '''
+
